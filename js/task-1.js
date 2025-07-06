7ab76d33f5cf4676872924b524522f2dd4f9e873
mad-jules <yuliia.pechera@gmail.com>
2025-07-06 15:11:38 +0300

    add task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,29 +1,12 @@
-const customer = {
-  username: 'Mango',
-  balance: 24000,
-  discount: 0.1,
-  orders: ['Burger', 'Pizza', 'Salad'],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-};
+const categories = document.querySelector("#categories");
+const categoriesList = categories.children;
+console.log("Number of categories:", categoriesList.length);
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, 'Steak');
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+const normalizedNodeList = [...categoriesList];
+normalizedNodeList.forEach((element) => {
+  const title = element.querySelector("h2").textContent;
+  console.log("Category:", title);
+
+  const items = element.querySelectorAll("ul > li");
+  console.log("Elements:", items.length);
+});
